48972f4544a1132b5f3032e352725e19c8ea350c
Xia-Youmei <xiayoumei666@163.com>
2022-10-08 11:50:52 +0800

    can_install

--- R/zzz.R
+++ R/zzz.R
@@ -13,17 +13,7 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-#' Add 100 to the given number
-#'
-#' @param x a number, float or int
-#'
-#' @return a number
-#' @export
-#'
-#' @examples
-#' add100(5)
-#' add100(-90)
-add100 <- function(x) {
-  x + 100
+hello <- function() {
+  print("Hello, world!")
 }
 
